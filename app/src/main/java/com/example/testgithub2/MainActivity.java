01e1e0f9ba9330ff951d3241399125d1cf144edb
Hossein <hs.masoumi37@gmail.com>
2019-02-02 09:23:49 +0330

    for loop added

--- app/src/main/java/com/example/testgithub2/MainActivity.java
+++ app/src/main/java/com/example/testgithub2/MainActivity.java
@@ -11,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     //test branch 2
+
+    for (int i = 0; i < 10; i++) {
+      i++;
+    }
+
+
   }
 }
