051c29a2f5d55d3c25142dc566c97bf8aa05e0f8
Hossein <hs.masoumi37@gmail.com>
2019-02-02 13:35:18 +0330

    update float2

--- app/src/main/java/com/example/testgithub2/MainActivity.java
+++ app/src/main/java/com/example/testgithub2/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     int number  = 2;
     int number2  = 2;
     float n = 2.5f;
-    float n2 = 3.5f;
+    float n2 = 4.6f;
 
     double m = 3.7687678;
 
