ba8e6eb7ee6066179ad13fb0ae5a27a9f89e3e29
Hossein <hs.masoumi37@gmail.com>
2019-02-05 20:00:21 +0330

    int num added

--- app/src/main/java/com/example/testgithub2/MainActivity.java
+++ app/src/main/java/com/example/testgithub2/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.testgithub2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+  private static final String TAG = "MainActivity";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    String test = "test";
+
+    int num = 2;
+
+    if (num == 3) {
+
+      Log.i(TAG, "onCreate: " + num);
+    }
 
   }
 }
