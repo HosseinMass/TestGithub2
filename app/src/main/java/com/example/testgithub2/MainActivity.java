1b5beec7b8bef4dbb7839d38c14fafe5a96e3a7a
Hossein <hs.masoumi37@gmail.com>
2019-09-30 09:01:36 +0330

    kkkk

--- app/src/main/java/com/example/testgithub2/MainActivity.java
+++ app/src/main/java/com/example/testgithub2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.testgithub2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    TextView textView = findViewById(R.id.txtTest);
+
+    textView.setText("salam");
+
+//    TestClass testClass = new TestClass();
+//    testClass.updateMethod(this,textView);
+
+    for (int i = 0; i < 20; i++) {
+
+      Log.i(TAG, "updateMethod: " + i);
+      try {
+        Thread.sleep(1000);
+        textView.setText("alo" + i);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
     //s
     //s
   }
--- app/src/main/java/com/example/testgithub2/TestClass.java
+++ app/src/main/java/com/example/testgithub2/TestClass.java
@@ -1,11 +1,35 @@
 package com.example.testgithub2;
 
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
 /**
  * Created by ${hosseiiin} on 2/2/2019.
  */
 public class TestClass {
+  private static final String TAG = "TestClass";
 
-  public void updateMethod(){
+  public void updateMethod(Activity activity, final TextView button) {
+
+    activity.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        for (int i = 0; i < 20; i++) {
+
+          Log.i(TAG, "updateMethod: " + i);
+          try {
+            Thread.sleep(1000);
+            button.setText("alo" + i);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+
+      }
+    });
 
 
   }
